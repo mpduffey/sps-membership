87bc0128dfc7a1727a7e25fadaf1c5b44178d41e
Mike Duffey <mpduffey@gmail.com>
2016-09-21 02:37:22 +0000

    generify

--- sps-membership.js
+++ sps-membership.js
@@ -11,25 +11,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var SpsMembership = (function () {
     function SpsMembership() {
-        this.people = [
-            { img: { src: "img/tgt.jpg", bgposition: '-13px' }, name: "Tommy G. Thompson", title: "Secretary of Health and Human Services" },
-            { img: { src: "img/snow.jpg" }, name: "John Snow", title: "Secretary of the Treasury" },
-            { img: { src: "img/meese.jpg" }, name: "Ed Meese", title: "Attorney General" },
-            { img: { src: "img/tgt.jpg" }, name: "Mike Duffey", title: "Special Assistant, DoD" },
-            { img: { src: "img/tgt.jpg" }, name: "Morgan Duffey", title: "Special Assistant, White House Homeland Security Council" },
-            { img: { src: "img/tgt.jpg" }, name: "Tom Alexander", title: "Director, DoD" },
-            { img: { src: "img/tgt.jpg" }, name: "Matt O'Malley", title: "Special Assistant, DoD" },
-            { img: { src: "img/tgt.jpg" }, name: "Anthony Murphy", title: "Director, DoD" },
-            { img: { src: "img/tgt.jpg" }, name: "Ducky Hemenway", title: "Deputy White House Liaison, DoD" },
-            { img: { src: "img/tgt.jpg" }, name: "Brandt Pasco", title: "Special Assistant, DoD" },
-            { img: { src: "img/tgt.jpg" }, name: "Thomas Bezas", title: "Special Assistant, DoD" },
-            { img: { src: "img/tgt.jpg" }, name: "Marie Vachon", title: "Director, White House" },
-        ];
     }
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Object)
+    ], SpsMembership.prototype, "membership", void 0);
     SpsMembership = __decorate([
         core_1.Component({
             selector: 'sps-membership',
-            template: "\n\t\t<section id=\"membership\" class=\"section-padding white-bg text-center\">\n\t\t\t<div class=\"container mb-60\">\n\t\t\t\t<div class=\"row text-center\">\n\t\t\t\t\t<div *ngFor=\"let person of people\" class=\"col-md-3\" style=\"height: 70px\">\n\t\t\t\t\t\t<div class=\"col-md-4\"><div class=\"img-circle\" [ngStyle]=\"{'background-image': 'url(' + person.img.src +')', 'background-position': person.img.bgposition}\"></div></div>\n\t\t\t\t\t\t<div class=\"col-md-8 align-left\"><p>{{person.name}}</p><p class=\"title\">{{person.title}}</p></div>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</section>\n\t",
+            template: "\n\t\t<section id=\"membership\" class=\"section-padding white-bg text-center\">\n\t\t\t<div class=\"container mb-60\">\n\t\t\t\t<div class=\"row text-center\">\n\t\t\t\t\t<div *ngFor=\"let person of membership.members\" class=\"col-md-3\" style=\"height: 70px\">\n\t\t\t\t\t\t<div class=\"col-md-4\"><div class=\"img-circle\" [ngStyle]=\"{'background-image': 'url(' + person.img.src +')', 'background-position': person.img.bgposition}\"></div></div>\n\t\t\t\t\t\t<div class=\"col-md-8 align-left\"><p>{{person.name}}</p><p class=\"title\">{{person.title}}</p></div>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</section>\n\t",
             styles: ["\n\t\tp {margin-bottom: 0;}\n\t\t.align-left {text-align: left;}\n\t\t.title {font-size: 9px;}\n\t\t.img-circle {background-size:cover; background-repeat:no-repeat; width:50px; height:50px;}\n\t"]
         }), 
         __metadata('design:paramtypes', [])
--- sps-membership.ts
+++ sps-membership.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
 	selector:				'sps-membership',
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
 		<section id="membership" class="section-padding white-bg text-center">
 			<div class="container mb-60">
 				<div class="row text-center">
-					<div *ngFor="let person of people" class="col-md-3" style="height: 70px">
+					<div *ngFor="let person of membership.members" class="col-md-3" style="height: 70px">
 						<div class="col-md-4"><div class="img-circle" [ngStyle]="{'background-image': 'url(' + person.img.src +')', 'background-position': person.img.bgposition}"></div></div>
 						<div class="col-md-8 align-left"><p>{{person.name}}</p><p class="title">{{person.title}}</p></div>
 					</div>
@@ -23,18 +23,5 @@ import {Component} from '@angular/core';
 })
 
 export class SpsMembership {
-	people = [
-		{img: {src: "img/tgt.jpg", bgposition: '-13px'}, name: "Tommy G. Thompson", title: "Secretary of Health and Human Services"},
-		{img: {src: "img/snow.jpg"}, name: "John Snow", title: "Secretary of the Treasury"},
-		{img: {src: "img/meese.jpg"}, name: "Ed Meese", title: "Attorney General"},
-		{img: {src: "img/tgt.jpg"}, name: "Mike Duffey", title: "Special Assistant, DoD"},
-		{img: {src: "img/tgt.jpg"}, name: "Morgan Duffey", title: "Special Assistant, White House Homeland Security Council"},
-		{img: {src: "img/tgt.jpg"}, name: "Tom Alexander", title: "Director, DoD"},
-		{img: {src: "img/tgt.jpg"}, name: "Matt O'Malley", title: "Special Assistant, DoD"},
-		{img: {src: "img/tgt.jpg"}, name: "Anthony Murphy", title: "Director, DoD"},
-		{img: {src: "img/tgt.jpg"}, name: "Ducky Hemenway", title: "Deputy White House Liaison, DoD"},
-		{img: {src: "img/tgt.jpg"}, name: "Brandt Pasco", title: "Special Assistant, DoD"},
-		{img: {src: "img/tgt.jpg"}, name: "Thomas Bezas", title: "Special Assistant, DoD"},
-		{img: {src: "img/tgt.jpg"}, name: "Marie Vachon", title: "Director, White House"},
-	]
+	@Input() membership;
 }
